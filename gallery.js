16d81178579c774633ed1921d27aba8f4100ec9f
david-shakaya <devidoz666@gmail.com>
2020-11-18 11:01:16 +0200

    ++

--- gallery.js
+++ gallery.js
@@ -7,7 +7,7 @@ import galleryListImg from './gallery-items.js'
 
 const galleryRef = document.querySelector('.js-gallery'); // Находит UL в котором разметка галереи
 const lightboxRef = document.querySelector('.js-lightbox'); // Див с модалкой
-const lightboxImageRef = document.querySelector('.lightbox__image'); 
+const lightboxImageRef = document.querySelector('.lightbox__image');
 const btnCloseRef = document.querySelector('button[data-action="close-lightbox"]');
 const overlayRef = document.querySelector('div.lightbox__overlay');
 
@@ -15,7 +15,7 @@ const overlayRef = document.querySelector('div.lightbox__overlay');
 
 let numberImg = 0
 //  Добавляет в ДОМ-документ все елементы масива
-const  getMarkup =(arr) => arr.reduce((acc, img) => 
+const getMarkup = (arr) => arr.reduce((acc, img) =>
   acc + `<li class="gallery__item">
   <a
     class="gallery__link"
@@ -25,13 +25,13 @@ const  getMarkup =(arr) => arr.reduce((acc, img) =>
       class="gallery__image"
       src="${img.preview}"
       data-source="${img.original}"
-      data-number="${numberImg +=1}"
+      data-number="${numberImg += 1}"
       alt="${img.description}"
     />
   </a>
 </li>`
-  
-, '')
+
+  , '')
 galleryRef.innerHTML = getMarkup(galleryListImg)
 
 
@@ -40,48 +40,66 @@ galleryRef.innerHTML = getMarkup(galleryListImg)
 galleryRef.addEventListener('click', openModl)  // Слушатель клика, откр. модалку
 btnCloseRef.addEventListener('click', closesModal) // При клике на кнопку закр. модалку
 overlayRef.addEventListener('click', closesModal) // При клике на Оверлей закр. модалку
-  
+
 
 function openModl(event) {
   event.preventDefault();
 
+let numberPicture = +event.target.dataset.number
+  window.addEventListener('keydown', (e) => {    
+   if (e.code === 'ArrowRight') {
+     numberPicture += 1 
+     ;
+     if (numberPicture === +event.target.dataset.number +1 ) {  // Тут наверное нужно искать с помощю перебирающих методов
+    // lightboxImageRef.src = (numberPicture = event.target.dataset.number )      // Возможно подойдет .find
+       console.log( +event.target.dataset.number +1);
+  }
+  }
+  console.log(numberPicture); //--- Это номер дата атрибута открытой картинки.
+  }) 
+  
+  if (event) {
+    lightboxImageRef.src = event.target.dataset.source
+     
+  }
+ 
 
-  console.log(event.target.dataset.number); //--- Это номер дата атрибута открытой картинки. Нужно добавить слушатель во время отркития модалки. И типа если стрелка вправо то +1 влево -1
+  //  Нужно добавить слушатель во время отркития модалки. И типа если стрелка вправо то +1 влево -1
 
   window.addEventListener('keydown', keyPress)  //Вешаем слушатель для закрытия при нажитии искейп
 
   if (event.target.nodeName !== 'IMG') {
     return null
   }
-   
+
   lightboxRef.classList.add('is-open')
   const galleryImage = document.querySelector('.gallery__image');
 
-  changesAtributeSrc(event)
+  // changesAtributeSrc(event)
 
 }
 
 const keyPress = (ev) => {
-   if (ev.code === 'Escape') {
-       closesModal() 
-    }
-  
+  if (ev.code === 'Escape') {
+    closesModal()
+  }
+
 };
 
 
 function changesAtributeSrc(url) {    // меняет src  атрибут. Подставляет дата атрибут взятый с event.target.dataset.source  с адресом кртинки 
-  lightboxImageRef.src = url.target.dataset.source 
- 
+  lightboxImageRef.src = url.target.dataset.source
+
 }
 
-function closesModal() {  
-    window.removeEventListener('keydown', keyPress )   // Снимает с esc слушатель
-   lightboxRef.classList.remove('is-open')     //удаляет класс 
- lightboxImageRef.removeAttribute('src')       // Удаляет атрибут 
+function closesModal() {
+  window.removeEventListener('keydown', keyPress)   // Снимает с esc слушатель
+  lightboxRef.classList.remove('is-open')     //удаляет класс 
+  lightboxImageRef.removeAttribute('src')       // Удаляет атрибут 
 }
 
 
 // window.addEventListener('keydown', (e) => {
-  
+
 //  console.log( e.code);
 // })
